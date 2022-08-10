d280f5be9542f98a2f0bf408d6751d0759f83089
Felipe Nascimento <felipenasgomes@gmail.com>
2022-08-10 12:12:49 -0300

    adjust indent lines

--- lua/plugin-config/indent-guide-lines.lua
+++ lua/plugin-config/indent-guide-lines.lua
@@ -1,8 +1,5 @@
--- settings for both plugins:
--- 'lukas-reineke/indent-blankline.nvim'
--- 'Yggdroot/indentLine'
-
-vim.api.nvim_command("let g:indentLine_char = '⎸'")
-vim.api.nvim_command("let g:indentLine_fileTypeExclude = ['text', 'markdown', 'help']")
-vim.api.nvim_command("let g:indentLine_bufNameExclude = ['STARTIFY', 'NVIMTREE']")
-vim.api.nvim_command("let g:indent_blankline_extra_indent_level = -1")
+require("indent_blankline").setup {
+  char = "⋅",
+  context_char = "▏",
+  show_current_context = true,
+}
