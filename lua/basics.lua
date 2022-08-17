32b7a5d30ef5ac75ad3e5cf65e04c8a1c8b0b043
Felipe Nascimento <felipenasgomes@gmail.com>
2022-08-17 14:07:19 -0300

    uses multiple colorcolumn

--- lua/basics.lua
+++ lua/basics.lua
@@ -75,10 +75,10 @@ vim.o.ttimeoutlen = 10  -- ms to wait for a key code seq to complete
 -- ================= Misc ================= --
 
 vim.wo.wrap = false -- don't wrap long text into multiple lines
-vim.wo.cc = '81'
 vim.o.history = 10000 -- numbers of entries in history for ':' commands and search patterns (10000 = max)
 vim.o.updatetime = 100 -- used for CursorHold event (for document highlighting detection)
 vim.o.mouse = 'nv' -- allow mose in normal & visual mode
+vim.api.nvim_command [[let &colorcolumn="81,".join(range(101,999),",")]]
 
 -- allows hidden buffers
 -- this means that a modified buffer doesn't need to be saved when changing
