20f2a306ff3c14475b47a21ef5ebb0b4a7e7608c
Felipe Nascimento <felipenasgomes@gmail.com>
2022-10-22 18:18:51 -0300

    improve formating

--- lua/plugin-config/neoformat.lua
+++ lua/plugin-config/neoformat.lua
@@ -1,7 +1,9 @@
 vim.g.neoformat_try_node_exe = 1
 vim.g.neoformat_enabled_javascript = { "prettier" }
 vim.g.neoformat_enabled_typescript = { "prettier" }
+vim.g.neoformat_enabled_graphql = { "prettier" }
+vim.g.neoformat_enabled_prisma = { "prettier" }
+vim.g.neoformat_enabled_lua = { "stylua" }
+vim.g.neoformat_enabled_rust = { "rustfmt" }
 
-vim.api.nvim_command([[
-autocmd BufWritePre * try | undojoin | Neoformat | catch /E790/ | Neoformat | endtry
-]])
+vim.api.nvim_command([[autocmd BufWritePre * silent Neoformat]])
