7c73ef5ab7cb11f2203a693485a1c315b088d453
Felipe Nascimento <felipenasgomes@gmail.com>
2022-12-15 17:01:50 -0300

    fix formatting on save

--- lua/plugin-config/lsp.lua
+++ lua/plugin-config/lsp.lua
@@ -5,15 +5,15 @@ local opts = { noremap = true, silent = true }
 local xopts = { noremap = true, silent = true, expr = true }
 
 require("lsp-colors").setup({
-	Error = "#F44747",
-	Warning = "#FF8800",
-	Hint = "#4FC1FF",
-	Information = "#FFCC66",
+  Error = "#F44747",
+  Warning = "#FF8800",
+  Hint = "#4FC1FF",
+  Information = "#FFCC66",
 })
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "tsserver", "prismals", "svelte", "sumneko_lua", "rust_analyzer" },
+  ensure_installed = { "tsserver", "prismals", "svelte", "sumneko_lua", "rust_analyzer" },
 })
 
 vim.api.nvim_set_keymap("i", "<Tab>", "pumvisible() ? '<C-n>' : '<Tab>'", xopts)
@@ -23,38 +23,41 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<Leader>q", vim.diagnostic.setloclist, opts)
 
 local on_attach = function(_, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	local bopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gd", telescope.lsp_definitions, bopts)
-	vim.keymap.set("n", "gi", telescope.lsp_references, bopts)
-	vim.keymap.set("n", "gI", telescope.lsp_implementations, bopts)
-	vim.keymap.set("n", "gt", telescope.lsp_type_definitions, bopts)
-	vim.keymap.set("n", "gh", vim.lsp.buf.hover, bopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.rename, bopts)
-	vim.keymap.set("n", "ga", vim.lsp.buf.code_action, bopts)
-	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bopts)
-	vim.keymap.set("n", "<Leader>F", function()
-		vim.lsp.buf.format({ async = true })
-	end, bopts)
+  local bopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "gd", telescope.lsp_definitions, bopts)
+  vim.keymap.set("n", "gi", telescope.lsp_references, bopts)
+  vim.keymap.set("n", "gI", telescope.lsp_implementations, bopts)
+  vim.keymap.set("n", "gt", telescope.lsp_type_definitions, bopts)
+  vim.keymap.set("n", "gh", vim.lsp.buf.hover, bopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.rename, bopts)
+  vim.keymap.set("n", "ga", vim.lsp.buf.code_action, bopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bopts)
+  vim.keymap.set("n", "<Leader>F", function()
+    vim.lsp.buf.formatting()
+  end, bopts)
 end
 
 lspconfig.tsserver.setup({
-	on_attach = on_attach,
+  on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+    on_attach(client, bufnr)
+  end,
 })
 lspconfig.rust_analyzer.setup({
-	on_attach = on_attach,
+  on_attach = on_attach,
 })
 lspconfig.sumneko_lua.setup({
-	on_attach = on_attach,
-	settings = {
-		Lua = { diagnostics = { globals = { "vim", "use" } } },
-	},
+  on_attach = on_attach,
+  settings = {
+    Lua = { diagnostics = { globals = { "vim", "use" } } },
+  },
 })
 lspconfig.svelte.setup({
-	on_attach = on_attach,
+  on_attach = on_attach,
 })
 lspconfig.prismals.setup({
-	on_attach = on_attach,
+  on_attach = on_attach,
 })
--- lua/plugin-config/null-ls.lua
+++ lua/plugin-config/null-ls.lua
@@ -1,18 +1,17 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-	sources = {
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.rustfmt,
-		null_ls.builtins.formatting.prismaFmt,
-		null_ls.builtins.formatting.taplo,
-	},
+  sources = {
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.formatting.prismaFmt,
+    null_ls.builtins.formatting.taplo,
+  },
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		vim.lsp.buf.formatting_sync(nil, 1000)
-	end,
-	once = true,
+vim.api.nvim_create_autocmd("BufWritePre *", {
+  callback = function()
+    vim.lsp.buf.formatting_sync()
+  end,
 })
