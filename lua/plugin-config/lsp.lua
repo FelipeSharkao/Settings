702c22dd1ce914e1ecfc9d85485cbddb1eee6bdf
Felipe Nascimento <felipenasgomes@gmail.com>
2022-12-15 18:51:05 -0300

    add stylua and fix formatting disabling for LSPs

--- lua/plugin-config/lsp.lua
+++ lua/plugin-config/lsp.lua
@@ -1,19 +1,19 @@
-local telescope = require('telescope.builtin')
-local lspconfig = require('lspconfig')
+local telescope = require("telescope.builtin")
+local lspconfig = require("lspconfig")
 
 local opts = { noremap = true, silent = true }
 local xopts = { noremap = true, silent = true, expr = true }
 
 require("lsp-colors").setup({
-  Error = "#F44747",
-  Warning = "#FF8800",
-  Hint = "#4FC1FF",
-  Information = "#FFCC66",
+	Error = "#F44747",
+	Warning = "#FF8800",
+	Hint = "#4FC1FF",
+	Information = "#FFCC66",
 })
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "tsserver", "prismals", "svelte", "sumneko_lua", "rust_analyzer" },
+	ensure_installed = { "tsserver", "prismals", "svelte", "sumneko_lua", "rust_analyzer" },
 })
 
 vim.api.nvim_set_keymap("i", "<Tab>", "pumvisible() ? '<C-n>' : '<Tab>'", xopts)
@@ -23,48 +23,50 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<Leader>q", vim.diagnostic.setloclist, opts)
 
 local on_attach = function(_, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  local bopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set("n", "gd", telescope.lsp_definitions, bopts)
-  vim.keymap.set("n", "gi", telescope.lsp_references, bopts)
-  vim.keymap.set("n", "gI", telescope.lsp_implementations, bopts)
-  vim.keymap.set("n", "gt", telescope.lsp_type_definitions, bopts)
-  vim.keymap.set("n", "gh", vim.lsp.buf.hover, bopts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.rename, bopts)
-  vim.keymap.set("n", "ga", vim.lsp.buf.code_action, bopts)
-  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bopts)
-  vim.keymap.set("n", "<Leader>F", function()
-    vim.lsp.buf.formatting()
-  end, bopts)
+	local bopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gd", telescope.lsp_definitions, bopts)
+	vim.keymap.set("n", "gi", telescope.lsp_references, bopts)
+	vim.keymap.set("n", "gI", telescope.lsp_implementations, bopts)
+	vim.keymap.set("n", "gt", telescope.lsp_type_definitions, bopts)
+	vim.keymap.set("n", "gh", vim.lsp.buf.hover, bopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.rename, bopts)
+	vim.keymap.set("n", "ga", vim.lsp.buf.code_action, bopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bopts)
+	vim.keymap.set("n", "<Leader>F", function()
+		vim.lsp.buf.formatting()
+	end, bopts)
 end
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local on_attach_no_format = function(client, bufnr)
+	client.server_capabilities.documentFormattingProvider = false
+	on_attach(client, bufnr)
+end
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 lspconfig.tsserver.setup({
-  on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
-    on_attach(client, bufnr)
-  end,
-  capabilities = capabilities,
+	on_attach = on_attach_no_format,
+	capabilities = capabilities,
 })
 lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
+	on_attach = on_attach_no_format,
+	capabilities = capabilities,
 })
 lspconfig.sumneko_lua.setup({
-  on_attach = on_attach,
-  settings = {
-    Lua = { diagnostics = { globals = { "vim", "use" } } },
-  },
-  capabilities = capabilities,
+	on_attach = on_attach_no_format,
+	settings = {
+		Lua = { diagnostics = { globals = { "vim", "use" } } },
+	},
+	capabilities = capabilities,
 })
 lspconfig.svelte.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 lspconfig.prismals.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
--- lua/plugin-config/nvim-cmp.lua
+++ lua/plugin-config/nvim-cmp.lua
@@ -1,46 +1,46 @@
-local cmp = require 'cmp'
+local cmp = require("cmp")
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<Down>'] = cmp.mapping.select_next_item(),
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<Up>'] = cmp.mapping.select_prev_item(),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-  }, {
-    { name = 'buffer' },
-  })
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<Down>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<Up>"] = cmp.mapping.select_prev_item(),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
+	}, {
+		{ name = "buffer" },
+	}),
 })
 
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
