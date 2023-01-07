f82703aca486fa1b4d3c35548aed63555e9c0100
Felipe Nascimento <felipenasgomes@gmail.com>
2023-01-06 22:37:27 -0300

    migrate from Packer to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,109 +1,100 @@
-local fn = vim.fn
-local installPath = DATA_PATH .. "/site/pack/packer/start/packer.nvim"
-
--- install packer if it's not installed already
-local packerBootstrap = nil
-if fn.empty(fn.glob(installPath)) > 0 then
-	packerBootstrap =
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", installPath })
-	vim.cmd([[packadd packer.nvim]])
+-- install lazy.nvim if it's not installed already
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer = require("packer").startup(function(use)
-	-- Packer should manage itself
-	use("wbthomason/packer.nvim")
-
+require("lazy").setup({
 	-- surround vim
-	use("tpope/vim-surround")
+	"tpope/vim-surround",
 
 	-- status line
-	use("glepnir/galaxyline.nvim")
+	"glepnir/galaxyline.nvim",
 
 	-- show recent files on empty nvim command
-	use("mhinz/vim-startify")
+	"mhinz/vim-startify",
 
 	-- floating terminal
-	use("voldikss/vim-floaterm")
+	"voldikss/vim-floaterm",
 
 	-- LSP and linters
-	use("folke/lsp-colors.nvim")
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig")
+	"folke/lsp-colors.nvim",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
 
 	-- Debugging
-	use("mfussenegger/nvim-dap")
-	use("mxsdev/nvim-dap-vscode-js")
+	"mfussenegger/nvim-dap",
+	"mxsdev/nvim-dap-vscode-js",
 
 	-- Linters and formatters
-	use("jose-elias-alvarez/null-ls.nvim")
+	"jose-elias-alvarez/null-ls.nvim",
 
 	-- Suggestions and completion
-	use("hrsh7th/vim-vsnip")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-vsnip")
+	"hrsh7th/vim-vsnip",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/cmp-vsnip",
 
 	-- telescope - searching / navigation
-	use({
+	{
 		"nvim-telescope/telescope.nvim",
-		requires = {
+		dependencies = {
 			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 			{ "nvim-telescope/telescope-project.nvim" },
 			{ "nvim-telescope/telescope-dap.nvim" },
 			{ "nvim-telescope/telescope-ui-select.nvim" },
 		},
-	})
+	},
 
 	-- better highlighting
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("evanleck/vim-svelte")
-	use("preservim/vim-markdown")
+	{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
+	"evanleck/vim-svelte",
+	"preservim/vim-markdown",
 
 	-- File tree window
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = { "nvim-tree/nvim-web-devicons" },
-	})
+	{ "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
 
 	-- prettier tabs
-	use("romgrk/barbar.nvim")
+	"romgrk/barbar.nvim",
 
 	-- Show commit messages
-	use("APZelos/blamer.nvim")
+	"APZelos/blamer.nvim",
 
 	-- show indentation levels
-	use("lukas-reineke/indent-blankline.nvim")
+	"lukas-reineke/indent-blankline.nvim",
 
 	-- improve folding
-	use("anuvyklack/pretty-fold.nvim")
+	"anuvyklack/pretty-fold.nvim",
 
 	-- Github Copilot
-	use("github/copilot.vim")
+	"github/copilot.vim",
 
 	-- Multicursor
-	use("mg979/vim-visual-multi")
+	"mg979/vim-visual-multi",
 
 	-- Automaticly close () [] {} '' ""
-	use("jiangmiao/auto-pairs")
+	"jiangmiao/auto-pairs",
 
 	-- colorscheme
-	use({ "rose-pine/neovim", as = "rose-pine", tag = "v1.*" })
-	use("nyoom-engineering/oxocarbon.nvim")
+	{ "rose-pine/neovim", name = "rose-pine", varsion = "v1.*" },
+	"nyoom-engineering/oxocarbon.nvim",
 
 	-- Connect to Discord's RPC
-	use("andweeb/presence.nvim")
-
-	-- this will automatically install listed dependencies
-	-- only the first time NeoVim is opened, because that's when Packer gets installed
-	if packerBootstrap then
-		require("packer").sync()
-	end
-end)
+	"andweeb/presence.nvim",
+})
 
 -- plugin specific configs go here
 require("plugin-config/telescope")
@@ -123,5 +114,3 @@ require("plugin-config/nvim-tree")
 require("plugin-config/autopairs")
 require("plugin-config/theme")
 require("plugin-config/discord")
-
-return packer
