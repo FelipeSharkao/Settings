9dcb7cf0575c096cffefd7947c6a7acb8711bfec
Felipe Nascimento <felipenasgomes@gmail.com>
2023-02-11 14:06:51 -0300

    fix keymaps for paste on neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,13 +1,16 @@
 if vim.g.neovide then
-	vim.opt.guifont = "Monocraft Nerd Font:h10"
+    vim.opt.guifont = "Monocraft Nerd Font:h10"
 
-	vim.g.neovide_scroll_animation_length = 0.2
-	vim.g.neovide_input_use_logo = false
-	vim.g.neovide_input_macos_alt_is_meta = true
+    vim.g.neovide_scroll_animation_length = 0.2
+    vim.g.neovide_input_use_logo = false
+    vim.g.neovide_input_macos_alt_is_meta = true
 
-	-- Paste in insert mode
-	vim.api.nvim_set_keymap("i", "<C-V>", '<C-R>"', { noremap = true })
-	vim.api.nvim_set_keymap("c", "<C-V>", '<C-R>"', { noremap = true })
-	vim.api.nvim_set_keymap("i", "<C-S-V>", "<C-R>+", { noremap = true })
-	vim.api.nvim_set_keymap("c", "<C-S-V>", "<C-R>+", { noremap = true })
+    -- Paste in insert mode
+    for _, mode in ipairs({ "i", "t" }) do
+        vim.api.nvim_set_keymap(mode, "<C-V>", "<Esc>gpi", { noremap = true })
+        vim.api.nvim_set_keymap(mode, "<C-S-V>", '<Esc>"+gpi', { noremap = true })
+    end
+
+    vim.api.nvim_set_keymap("c", "<C-V>", '<C-R>"', { noremap = true })
+    vim.api.nvim_set_keymap("c", "<C-S-V>", "<C-R>+", { noremap = true })
 end
