e98c95d9c9d16f8aef313fdb4beaec3b6143f2ea
Felipe Nascimento <felipenasgomes@gmail.com>
2023-02-13 17:04:58 -0300

    setup nvim-scrollbar

--- lua/plugin-config/nvim-scrollbar.lua
+++ lua/plugin-config/nvim-scrollbar.lua
@@ -1,1 +1,22 @@
-require("scrollbar").setup()
+require("scrollbar").setup({
+    handle = {
+        hide_if_all_visible = false,
+    },
+    marks = {
+        GitAdd = {
+            text = "▎",
+        },
+        GitChange = {
+            text = "▎",
+        },
+        GitDelete = {
+            text = "▎",
+        },
+    },
+    handlers = {
+        cursor = false,
+        diagnostics = true,
+        gitsigns = true,
+    },
+})
+require("scrollbar.handlers.gitsigns").setup()
