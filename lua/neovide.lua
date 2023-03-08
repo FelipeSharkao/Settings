bb1b7589f30a70d9a5b042f76db6b5091bd1887b
Felipe Nascimento <felipenasgomes@gmail.com>
2023-03-08 18:03:21 -0300

    fix paste in terminal mode

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,3 +1,5 @@
+local keymap = vim.keymap.set
+
 if vim.g.neovide then
     vim.opt.guifont = "Monocraft Nerd Font:h10"
 
@@ -5,12 +7,13 @@ if vim.g.neovide then
     vim.g.neovide_input_use_logo = false
     vim.g.neovide_input_macos_alt_is_meta = true
 
-    -- Paste in insert mode
-    for _, mode in ipairs({ "i", "t" }) do
-        vim.api.nvim_set_keymap(mode, "<C-V>", "<Esc>gpi", { noremap = true })
-        vim.api.nvim_set_keymap(mode, "<C-S-V>", '<Esc>"+gpi', { noremap = true })
-    end
+    -- Paste
+    keymap("i", "<C-V>", "<Esc>gpi", { noremap = true })
+    keymap("i", "<C-S-V>", '<Esc>"+gpi', { noremap = true })
 
-    vim.api.nvim_set_keymap("c", "<C-V>", '<C-R>"', { noremap = true })
-    vim.api.nvim_set_keymap("c", "<C-S-V>", "<C-R>+", { noremap = true })
+    keymap("t", "<C-V>", "<C-\\><C-N>gpi", { noremap = true })
+    keymap("t", "<C-S-V>", '<C-\\><C-N>"+gpi', { noremap = true })
+
+    keymap("c", "<C-V>", '<C-R>"', { noremap = true })
+    keymap("c", "<C-S-V>", "<C-R>+", { noremap = true })
 end
