6e99ac23af58633238a4bfa0571922f8a667878c
Felipe Nascimento <felipenasgomes@gmail.com>
2023-03-25 13:41:42 -0300

    dont keep files tree always opened

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -29,24 +29,7 @@ require("nvim-web-devicons").setup({
 
 local api = require("nvim-tree.api")
 
-local function open_nvim_tree(data)
-    -- buffer is a real file on the disk
-    local real_file = vim.fn.filereadable(data.file) == 1
-
-    -- buffer is a [No Name]
-    local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
-    if not real_file and not no_name then
-        return
-    end
-
-    -- open the tree, find the file but don't focus it
-    api.tree.toggle({ focus = false, find_file = true })
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
 local opts = { silent = true, noremap = true }
 vim.keymap.set("n", ";n", function()
-    api.tree.open({ find_file = true })
+    api.tree.toggle({ find_file = true })
 end, opts)
