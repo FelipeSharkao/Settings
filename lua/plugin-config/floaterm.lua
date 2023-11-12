7451c598f153d9b31c4846437aebbf52308ae284
Felipe Nascimento <felipenasgomes@gmail.com>
2023-11-12 18:16:24 -0300

    set default floaterm size

--- lua/plugin-config/floaterm.lua
+++ lua/plugin-config/floaterm.lua
@@ -1,6 +1,13 @@
-vim.api.nvim_create_user_command("Git", "FloatermNew --cwd=<buffer> --width=0.8 --height=0.8 lazygit", {})
+vim.g.floaterm_width = 0.9
+vim.g.floaterm_height = 0.9
+
+vim.api.nvim_create_user_command("Git", "FloatermNew --cwd=<buffer> lazygit", {})
 vim.api.nvim_create_user_command("Run", "FloatermNew --autoclose=0 <args>", { nargs = "*" })
-vim.api.nvim_create_user_command("RunHere", "FloatermNew --autoclose=0 --cwd=<buffer> <args>", { nargs = "*" })
+vim.api.nvim_create_user_command(
+    "RunHere",
+    "FloatermNew --autoclose=0 --cwd=<buffer> <args>",
+    { nargs = "*" }
+)
 
 -- CTRL-q to close term
 vim.api.nvim_set_keymap("t", "<C-q>", "<Cmd>FloatermKill<CR>", { noremap = true, silent = true })
