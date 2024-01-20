762c9acbd7e43642e1134ff7539b0b3c472a24d5
FelipeSharkao <felipenasgomes@gmail.com>
2024-01-20 14:51:11 -0300

    wezterm: remove window padding

--- WezTerm/wezterm.lua
+++ WezTerm/wezterm.lua
@@ -32,6 +32,13 @@ config.keys = {
     },
 }
 
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
 config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.8
 
