039858386619d2c67c4b114161fdd65d8222d0f5
FelipeSharkao <felipenasgomes@gmail.com>
2024-04-22 18:53:13 -0300

    nvim: split right and bottom instead of left and top

--- Nvim/lua/basics.lua
+++ Nvim/lua/basics.lua
@@ -13,14 +13,16 @@ vim.o.writebackup = false
 vim.o.autoread = true -- auto file change detection
 
 -- Triger `autoread` when files changes on disk
-vim.api.nvim_create_autocmd(
-    { "FocusGained", "BufEnter" },
-    {
-        callback = function()
-            vim.cmd("silent! checktime")
-        end
-    }
-)
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+    callback = function()
+        vim.cmd("silent! checktime")
+    end,
+})
+
+-- ================= Split ================= --
+
+vim.o.splitright = true -- open new vertical split to the right
+vim.o.splitbelow = true -- open new horizontal split below
 
 -- ================= Scrolling ================= --
 
@@ -28,13 +30,13 @@ vim.o.scrolloff = 8 -- start scrolling when 8 lines away from margins
 
 -- ================= Indentation ================= --
 
-vim.o.tabstop = 4        -- maximum width of tab character (measured in spaces)
-vim.o.shiftwidth = 4     -- size of indent (measured in spaces), should equal tabstop
-vim.o.softtabstop = 4    -- should be the same as the other two above
-vim.o.expandtab = true   -- expand tabs to spaces
+vim.o.tabstop = 4 -- maximum width of tab character (measured in spaces)
+vim.o.shiftwidth = 4 -- size of indent (measured in spaces), should equal tabstop
+vim.o.softtabstop = 4 -- should be the same as the other two above
+vim.o.expandtab = true -- expand tabs to spaces
 vim.o.smartindent = true -- smart indenting on new line for C-like programs
-vim.o.autoindent = true  -- copy the indentation from previous line
-vim.o.smarttab = true    -- tab infront of a line inserts blanks based on shiftwidth
+vim.o.autoindent = true -- copy the indentation from previous line
+vim.o.smarttab = true -- tab infront of a line inserts blanks based on shiftwidth
 
 -- ================= Number column ================= --
 
@@ -46,9 +48,9 @@ vim.o.numberwidth = 3 -- width of number column, 3 for 999 lines
 -- ================= Search ================= --
 
 vim.o.ignorecase = true -- Ignorecase when searching
-vim.o.incsearch = true  -- start searching on each keystroke
-vim.o.smartcase = true  -- ignore case when lowercase, match case when capital case is used
-vim.o.hlsearch = true   -- highlight the search results
+vim.o.incsearch = true -- start searching on each keystroke
+vim.o.smartcase = true -- ignore case when lowercase, match case when capital case is used
+vim.o.hlsearch = true -- highlight the search results
 
 -- ================= Performance ================= --
 
@@ -89,9 +91,9 @@ vim.o.pumblend = 15
 
 -- ================= Misc ================= --
 
-vim.o.history = 10000       -- numbers of entries in history for ':' commands and search patterns (10000 = max)
-vim.o.updatetime = 100      -- used for CursorHold event (for document highlighting detection)
-vim.o.mouse = "nv"          -- allow mouse in normal & visual mode
+vim.o.history = 10000 -- numbers of entries in history for ':' commands and search patterns (10000 = max)
+vim.o.updatetime = 100 -- used for CursorHold event (for document highlighting detection)
+vim.o.mouse = "nv" -- allow mouse in normal & visual mode
 vim.o.mousemodel = "extend" -- right click extends selection
 
 -- allows hidden buffers
