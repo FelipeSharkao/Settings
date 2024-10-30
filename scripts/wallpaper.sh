3eedd0ef9bb819d8e9ecec9c876dc43ebd44a418
FelipeSharkao <felipenasgomes@gmail.com>
2024-10-30 09:23:32 -0300

    hypr: improve wallpaper script

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,18 +1,26 @@
 WALLPAPER=$(find "$1" -type f | shuf -n 1)
 
+echo "Setting swaybg..."
 killall swaybg &> /dev/null
 swaybg --image "$WALLPAPER" --mode fill &> /dev/null & disown
 
+echo "Setting gnome background..."
 gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER" 2> /dev/null
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER" 2> /dev/null
 
+echo "Running wallust..."
 wallust run "$WALLPAPER"
 
+echo "Resetting nwg-panel..."
 killall nwg-panel &> /dev/null
 nwg-panel &> /dev/null & disown
 
+echo "Resetting mako..."
 killall mako &> /dev/null
 mako &> /dev/null & disown
 
-pywalfox update &> /dev/null & disown
+echo "Uptating pywalfox..."
+pywalfox update 2> /dev/null & disown
+
+echo "Notifying neovim..."
 nvr --serverlist | xargs -I{} zsh -c 'nvr --nostart --servername "{}" +"colorscheme wal" || true' &> /dev/null & disown
