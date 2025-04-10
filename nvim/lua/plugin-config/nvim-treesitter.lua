2bc273ac3cbb7bec2517b08bf8e021bcf41df7ec
FelipeSharkao <felipenasgomes@gmail.com>
2025-04-10 16:55:23 -0300

    nvim: I don't even remember why I disabled this

--- nvim/lua/plugin-config/nvim-treesitter.lua
+++ nvim/lua/plugin-config/nvim-treesitter.lua
@@ -2,6 +2,6 @@ require("nvim-treesitter.configs").setup({
     -- will install treesitter for all available languages
     ensure_installed = "all",
     highlight = { enable = true },
-    indent = { enable = false },
+    indent = { enable = true },
     endwise = { enable = true },
 })
