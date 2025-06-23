51105d15c6f8ea0cfebbb8e3ade7ce231bc74fcf
FelipeSharkao <felipenasgomes@gmail.com>
2025-06-23 00:00:54 -0300

    nvim: colorpicker

---
+++ nvim/lua/plugins/ccc.lua
@@ -0,0 +1,47 @@
+local ft = {
+    "css",
+    "scss",
+    "sass",
+    "stylus",
+    "htmtl",
+    "javascriptreact",
+    "typescriptreact",
+    "svelte",
+    "astro",
+}
+
+---@type LazySpec[]
+return {
+    {
+        "uga-rosa/ccc.nvim",
+        event = { "BufEnter", "BufWinEnter" },
+        ft = ft,
+        opts = function()
+            local ccc = require("ccc")
+            return {
+                highlighter = {
+                    auto_enable = true,
+                    filetypes = ft,
+                },
+                inputs = {
+                    ccc.input.rgb,
+                    ccc.input.hsl,
+                },
+                pickers = {
+                    ccc.picker.hex,
+                    ccc.picker.css_rgb,
+                    ccc.picker.css_hsl,
+                    ccc.picker.css_names,
+                },
+                recognize = { input = true, output = true },
+            }
+        end,
+        cmd = {
+            "CccPick",
+            "CccConvert",
+            "CccHighlighterEnable",
+            "CccHighlighterDisable",
+            "CccHighlighterToggle",
+        },
+    },
+}
