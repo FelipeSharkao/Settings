85434476c67dfffceb7485579bfd4233e198c90d
FelipeSharkao <felipenasgomes@gmail.com>
2025-07-29 10:29:52 -0300

    nvim: no merges in file history

--- nvim/lua/plugin-config/git.lua
+++ nvim/lua/plugin-config/git.lua
@@ -4,4 +4,7 @@ require("diffview").setup({
     view = {
         merge_tool = { layout = "diff4_mixed" },
     },
+    default_args = {
+        DiffviewFileHistory = { "--no-merges" },
+    },
 })
