5edcdaf1218aa703d93713a5658d498b68d7f5a6
FelipeSharkao <felipenasgomes@gmail.com>
2025-07-29 13:25:31 -0300

    wlogout: notification if hibernation fails

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -3,12 +3,26 @@ MESSAGE="Bem vindo de volta, $FULL_NAME!"
 
 case "$1" in
     suspend)
-        systemctl suspend
-        physlock -d -p "$MESSAGE"
+        if systemctl suspend; then
+            physlock -d -p "$MESSAGE"
+        else
+            notify-send              \
+                -i system-suspend    \
+                -a lock              \
+                "Falha ao suspender" \
+                "Não foi possível suspender."
+        fi
         ;;
     hibernate)
-        systemctl hibernate
-        physlock -d -p "$MESSAGE"
+        if systemctl hibernate; then
+            physlock -d -p "$MESSAGE"
+        else
+            notify-send             \
+                -i system-suspend   \
+                -a lock             \
+                "Falha ao hibernar" \
+                "Não foi possível hibernar. Verifique se há memória suficiente."
+        fi
         ;;
     "")
         physlock -p "$MESSAGE"
@@ -18,3 +32,4 @@ case "$1" in
         exit 2
         ;;
 esac
+
