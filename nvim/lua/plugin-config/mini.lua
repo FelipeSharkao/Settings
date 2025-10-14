42d7902471c1d9a43bb11ed3f399b65afc3dd82e
FelipeSharkao <felipenasgomes@gmail.com>
2025-10-14 09:37:00 -0300

    nvim: more compact mini.statusline

--- nvim/lua/plugin-config/mini.lua
+++ nvim/lua/plugin-config/mini.lua
@@ -36,7 +36,46 @@ require("mini.pick").setup({
 })
 
 -- Opinated statusline
-require("mini.statusline").setup()
+local statusline = require("mini.statusline")
+
+local function statusline_content()
+    -- This was copied from the default content, mostly to remove some stuff, since it
+    -- clips the filename in split windows
+    local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+    local git = statusline.section_git({ trunc_width = 40 })
+    -- local diff = statusline.section_diff({ trunc_width = 75 })
+    local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+    -- local lsp = statusline.section_lsp({ trunc_width = 75 })
+    local filename = statusline.section_filename({ trunc_width = 140 })
+    local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+    local location = statusline.section_location({ trunc_width = 75 })
+    local search = statusline.section_searchcount({ trunc_width = 75 })
+
+    return statusline.combine_groups({
+        { hl = mode_hl, strings = { mode } },
+        {
+            hl = "MiniStatuslineDevinfo",
+            strings = {
+                git,
+                -- diff,
+                diagnostics,
+                -- lsp,
+            },
+        },
+        "%<", -- Mark general truncate point
+        { hl = "MiniStatuslineFilename", strings = { filename } },
+        "%=", -- End left alignment
+        { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+        { hl = mode_hl, strings = { search, location } },
+    })
+end
+
+statusline.setup({
+    content = {
+        active = statusline_content,
+        inactive = statusline_content,
+    },
+})
 
 -- Opinated tabline
 require("mini.tabline").setup()
