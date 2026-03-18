0c4c03a396ead1baa396d189c28e5df88661a847
FelipeSharkao <felipenasgomes@gmail.com>
2026-03-18 08:03:50 -0300

    nvim: update nvim-treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,25 +2,21 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "c",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "query",
-                "vim",
-                "vimdoc",
-                "javascript",
-                "typescript",
-            },
-            auto_install = true,
-            sync_install = false,
-            ignore_install = {},
-            highlight = { enable = true },
-            indent = { enable = false },
-            endwise = { enable = true },
-            modules = {},
+        local ts = require("nvim-treesitter")
+        local parsers = require("nvim-treesitter.parsers")
+        ts.setup()
+
+        local augroup = vim.api.nvim_create_augroup("treesitter-config", { clear = true })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = augroup,
+            callback = function(args)
+                if parsers[args.match] then
+                    ts.install({ args.match }):await(function(err)
+                        if not err then vim.treesitter.start() end
+                    end)
+                end
+            end,
         })
     end,
 }
